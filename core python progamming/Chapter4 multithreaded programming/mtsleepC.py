cdd69e5a62914abc5e4311563dd50660bd075e1b
daye <2464512446@qq.com>
2018-11-13 16:40:43 +0800

    add thread

--- core python progamming/Chapter4 multithreaded programming/mtsleepC.py
+++ core python progamming/Chapter4 multithreaded programming/mtsleepC.py
@@ -1,1 +1,40 @@
 # coding:utf-8
+# 调用一个Thread实例，传给他一个函数
+import threading
+from time import ctime, sleep
+
+loops = [4.2]
+
+
+def loop(nloop,nsec):
+    """
+    :param nloop: loop函数的序列号，如loop0 ,loop1
+    :param nsec:  sleep的秒数
+    :return:
+    """
+    print("start loop ", nloop, "at:", ctime())
+    sleep(nsec)
+    print("loop", nloop, "done at:", ctime())
+
+
+def main():
+    print "startting at:", ctime()
+    threads = []
+    nloops = range(len(loops))
+
+    for i in nloops:
+        t = threading.Thread(target=loop, args=(i, loops[i]))
+        threads.append(t)
+        # 这个循环创建Thread实例，加入threads队列
+
+    for i in nloops:
+        threads[i].start()
+
+    for i in nloops:
+        threads[i].join()
+        # 直至启动的线程终止之前一直被挂起，除非给出了timeout,否则会一直阻塞
+    print("ALL DONE at:", ctime())
+
+
+if __name__ == '__main__':
+    main()
---
+++ core python progamming/Chapter4 multithreaded programming/mtsleepE.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+# 派生thread的子类，并创建子类实例
+import threading
+from time import ctime, sleep
+
+loops = [4, 2]
+
+# MyThread就是threading的子类，继承了了threading.Thread和他的属性，重写了run
+class MyThread(threading.Thread):
+    def __init__(self,func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.func(*self.args)
+
+
+def loop(nloop, nsec):
+    print('start loop', nloop, 'at:', ctime())
+    sleep(nsec)
+    print('loop', nloop, 'done at', ctime())
+
+
+def main():
+    print "starting at:", ctime()
+    threads = []
+    nloops = range(len(loops))
+
+    for i in nloops:
+        # 这里实例化的是Thread的子类的实例，而不是Thread的实例化
+        t = MyThread(loop, (i, loops[i]),
+                    loop.__name__)
+        threads.append(t)
+    for i in nloops:
+        threads[i].start()
+    for i in nloops:
+        threads[i].join()
+
+    print("ALL DONE at:",ctime())
+
+if __name__ == '__main__':
+    main()
