9d65f1d68794e15a2e224db6664013baf9cdb9ee
fank <30522208+fank-cd@users.noreply.github.com>
2018-11-29 15:40:57 +0800

    done Dijkstra

--- Grokking Algorithms/Chapter 7 Dijkstra/Dijkstra.py
+++ Grokking Algorithms/Chapter 7 Dijkstra/Dijkstra.py
@@ -1,2 +1,59 @@
 # coding:utf-8
 
+# 书中的代码似乎有错误， find_lowest_cost_node不应该找到最后的终点的节点，终点的节点没有邻居
+# 重点还是理解思想吧，讲的比较浅，代码也不是很溜。但是思想讲的不错的
+
+infinity = float("inf")
+
+
+costs ={}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+
+parents = {}
+parents["a"] =  "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] =  5
+
+graph["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node =  None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+
+
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
