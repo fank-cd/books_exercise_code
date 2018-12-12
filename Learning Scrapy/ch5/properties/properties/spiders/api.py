368d575e943099b81c48f7f68637f9026f955cd5
fank <30522208+fank-cd@users.noreply.github.com>
2018-12-12 14:38:17 +0800

    ..

---
+++ Learning Scrapy/ch5/properties/properties/spiders/api.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import urlparse
+from socket import gethostname
+from datetime import datetime
+
+import scrapy
+import json
+from scrapy.loader import ItemLoader
+from scrapy.http import Request
+from scrapy.loader.processors import MapCompose, Join
+
+from properties.items import PropertiesItem
+
+
+class ApiSpider(scrapy.Spider):
+    name = 'api'
+    allowed_domains = ['localhost']
+
+    start_urls = [
+        'http://localhost:9312/properties/api.json'
+    ]
+
+    def parse(self, response):
+
+        base_url = "ttp://localhost:9312/properties/"
+        js = json.load(response.body)
+        for item in js:
+            title = item["title"]
+            id = item["id"]
+            url = base_url + "property_%06d.html" % id
+            yield Request(url, meta={"title": title}, callback=self.parse_item)
+
+    def parse_item(self, response):
+        """
+        @url http://localhost:9312/properties/property_000001.html
+        @returns items 1
+        @scrapes title price description address image_urls
+        @scrapes url project spider server date
+        """
+
+        l = ItemLoader(item=PropertiesItem(), response=response)
+
+        # l.add_xpath('title', '//*[@itemprop="name"][1]/text()', MapCompose(unicode.strip, unicode.title))
+        l.add_value("title", response.meta["title"], MapCompose(unicode.strip, unicode.title))
+        l.add_xpath('price', './/*[@itemprop="price"][1]/text()', MapCompose(lambda i: i.replace(',', ''), float),
+                    re='[,.0-9]+')
+        l.add_xpath('description', '//*[@itemprop="description"][1]/text()', MapCompose(unicode.strip), Join())
+        l.add_xpath('address', '//*[@itemtype="http://schema.org/Place"][1]/text()', MapCompose(unicode.strip))
+        l.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src',
+                    MapCompose(lambda i: urlparse.urljoin(response.url, i)))
+
+        l.add_value('url', response.url)
+        l.add_value('project', self.settings.get('BOT_NAME'))
+        l.add_value('spider', self.name)
+        l.add_value('server', gethostname())
+        l.add_value('date', datetime.now())
+
+        return l.load_item()
