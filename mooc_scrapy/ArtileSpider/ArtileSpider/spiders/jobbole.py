3b3e525a6935d00763de72b499c40f08f13e5dc0
fank <30522208+fank-cd@users.noreply.github.com>
2018-12-19 16:33:54 +0800

    18/12/19

--- mooc_scrapy/ArtileSpider/ArtileSpider/spiders/jobbole.py
+++ mooc_scrapy/ArtileSpider/ArtileSpider/spiders/jobbole.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobboleSpider(scrapy.Spider):
@@ -9,4 +10,23 @@ class JobboleSpider(scrapy.Spider):
 
     def parse(self, response):
         res_selector = response.xpath('//div[@class="entry-header"]/h1/text()')
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract()[0]
+        create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().\
+            replace('·', "").strip()
+        praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(r".*(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.groups(1)
+        # pass
+
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(r".*(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.groups(1)
+
+        content = response.xpath("//div[@class='entry']").extract()[0]
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
         pass
