b56e5c90267166574a0201c38c67d75cd4cb2229
fank <30522208+fank-cd@users.noreply.github.com>
2019-01-10 17:47:37 +0800

    lagou spider

---
+++ mooc_scrapy/ArtileSpider/ArtileSpider/spiders/lagou.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class LagouSpider(CrawlSpider):
+    name = 'lagou'
+    allowed_domains = ['www.lagou.com']
+    start_urls = ['http://www.lagou.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_job', follow=True),
+    )
+
+
+    def parse_start_url(self, response):
+        return []
+
+    def process_results(self, response, results):
+        return results
+
+
+    def parse_job(self, response):
+        # 解析拉勾网的职位
+        i = {}
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
+
+
