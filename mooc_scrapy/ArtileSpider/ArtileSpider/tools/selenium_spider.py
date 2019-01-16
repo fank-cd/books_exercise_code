6a0c5dbccef933a23a2ce896b125492e985732d3
fank <30522208+fank-cd@users.noreply.github.com>
2019-01-16 17:38:37 +0800

    selnium login

--- mooc_scrapy/ArtileSpider/ArtileSpider/tools/selenium_spider.py
+++ mooc_scrapy/ArtileSpider/ArtileSpider/tools/selenium_spider.py
@@ -5,12 +5,6 @@ from time import sleep
 
 browser = webdriver.Firefox(executable_path="E:\\geckodriver.exe")
 
-
-
-
-
-
-
 # browser.find_element_by_xpath().send_keys()
 #browser.quit()
 # selenium完成微博模拟登录
@@ -21,3 +15,7 @@ sleep(10)
 browser.find_element_by_xpath(".//input[@id='loginname']").send_keys("18684028609")
 browser.find_element_by_xpath(".//div[@class='info_list password']/div/input[@class='W_input']").send_keys("Cdkc201301")
 browser.find_element_by_xpath(".//div[@class='info_list login_btn']/a[@node-type='submitBtn']").click()
+sleep(5)
+for i in range(3):
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage")
+    sleep(3)
