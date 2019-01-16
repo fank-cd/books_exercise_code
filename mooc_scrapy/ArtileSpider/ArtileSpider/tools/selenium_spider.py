329da09d70ca94d9b9d8944f239d0fd9a94f8de6
fank <30522208+fank-cd@users.noreply.github.com>
2019-01-16 17:29:37 +0800

    selnium login

--- mooc_scrapy/ArtileSpider/ArtileSpider/tools/selenium_spider.py
+++ mooc_scrapy/ArtileSpider/ArtileSpider/tools/selenium_spider.py
@@ -1,14 +1,23 @@
 # coding:utf-8
 
 from selenium import webdriver
-
+from time import sleep
 
 browser = webdriver.Firefox(executable_path="E:\\geckodriver.exe")
 
-browser.get("http://www.baidu.com")
 
-print(browser.page_source)
+
+
+
 
 
 # browser.find_element_by_xpath().send_keys()
-browser.quit()
+#browser.quit()
+# selenium完成微博模拟登录
+browser.get("https://weibo.com/login.php")
+print(browser.page_source)
+sleep(10)
+#browser.find_element_by_id("loginname").send_keys("18684028609")
+browser.find_element_by_xpath(".//input[@id='loginname']").send_keys("18684028609")
+browser.find_element_by_xpath(".//div[@class='info_list password']/div/input[@class='W_input']").send_keys("Cdkc201301")
+browser.find_element_by_xpath(".//div[@class='info_list login_btn']/a[@node-type='submitBtn']").click()
