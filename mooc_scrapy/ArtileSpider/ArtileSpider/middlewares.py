8343cf3208c5dc64a9b8097205f612014bcfa0a1
fank <30522208+fank-cd@users.noreply.github.com>
2019-01-21 17:14:15 +0800

    selnium login

--- mooc_scrapy/ArtileSpider/ArtileSpider/middlewares.py
+++ mooc_scrapy/ArtileSpider/ArtileSpider/middlewares.py
@@ -105,11 +105,12 @@ class ArtilespiderDownloaderMiddleware(object):
 
 class RandomUserAgentMiddlware(object):
     # 随机更换user-agent
-    def __init__(self,crawler):
+    def __init__(self, crawler):
         super(RandomUserAgentMiddlware, self).__init__()
         # self.user_agent_list = crawler.settings.get("user_agent_list",[])
         self.ua = UserAgent()
         self.ua_type = crawler.settings.get("RANDOM_UA_TYPE", "random")
+
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler)
@@ -117,4 +118,5 @@ class RandomUserAgentMiddlware(object):
     def process_request(self, request,spider):
         def get_ua():
             return getattr(self.ua, self.ua_type)
+        random_agent = get_ua()
         request.headers.setdefault("User-Agent", get_ua())
--- mooc_scrapy/ArtileSpider/ArtileSpider/settings.py
+++ mooc_scrapy/ArtileSpider/ArtileSpider/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'ArtileSpider.middlewares.ArtilespiderDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'ArtileSpider.middlewares.RandomUserAgentMiddlware': 543,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware':None,
+   # 'ArtileSpider.middlewares.ArtilespiderDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
