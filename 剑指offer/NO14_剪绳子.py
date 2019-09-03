3964a77467256def5320571045b790063b21f4a7
fukaifeng <2464512446@qq.com>
2019-09-03 14:39:26 +0800

    no14 贪婪法

--- 剑指offer/NO14_剪绳子.py
+++ 剑指offer/NO14_剪绳子.py
@@ -12,7 +12,7 @@ def max_solution(n):
     if n == 3:
         return 2
 
-    products = []
+    products = [0 for _ in range(n + 1)]
     products[0] = 0
     products[1] = 1
     products[2] = 2
@@ -22,6 +22,42 @@ def max_solution(n):
     for i in range(4, n + 1):
         for j in range(1, i // 2 + 1):
             product = products[j] * products[i - j]
-            if product > products[i]:
+            if product > products[i]:  # 得到products[i]的最优解
                 products[i] = product
     return products[n]
+
+print(max_solution(8))
+
+# 贪婪法
+# 如果我们按照如下的策略来剪绳子，则得到的各段绳子的长度的乘积
+# 将最大；当n大于等于5时，我们尽可能多地剪长度为3的绳子；当剩下的绳子
+# 长度为4时；把绳子剪成两端长度为2的绳子。这种思路对应的代码如下：
+
+# 当n 大于等于5的时候，我们可以证明2(n-2) > n 并且3(n-3)>n。也就是
+# 说，当绳子剩下的长度大于或者等于5的时候，我们就把它剪成3或者2的绳子
+# 段。另外，当n大于等于5时候，3(n-3)>=2(n-2)。所以我们应该尽可能地多
+# 剪长度位3的绳子段。
+
+ def max_product_after_cuting2(length):
+    """
+    贪婪法
+    时间 O(1)，空间O(1)
+    """
+    if not isinstance(length, int):
+        return
+
+    if length < 2:
+        return 0
+
+    if length == 2:
+        return 1
+
+    if length == 3:
+        return 2
+
+    times_of_3 = length // 3
+    if (length - 3 * times_of_3) == 1:
+        times_of_3 -= 1
+
+    times_of_2 = (length - 3 * times_of_3) / 2
+    return int((3 ** times_of_3) * (2 ** times_of_2))
