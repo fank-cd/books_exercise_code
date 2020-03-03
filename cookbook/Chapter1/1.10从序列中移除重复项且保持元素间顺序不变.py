e828c9db1e1dfbc96e5dd8452897b5ec28c2ea41
2464512446@qq.com <sslz200903>
2020-03-03 23:48:01 +0800

    1.10

---
+++ cookbook/Chapter1/1.10从序列中移除重复项且保持元素间顺序不变.py
@@ -0,0 +1,38 @@
+
+# 对于序列中的值是可哈希的，那么这个问题就可以使用集合和生成器解决
+# 可哈希的：如果一个对象是可哈希的，那么在它的生存期内必须是不变的，它需要有一个__hash__()方法。
+# 整数、浮点数、字符串、元组都是不可变的。
+
+
+def dequpe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+print(list(dequpe(a)))
+
+
+# 如果序列中的元素是不可哈希的，那又如何呢？
+# 比如元素是字典的情况
+
+def dequpe(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if not key else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+
+a = [
+    {"x": 1, "y": 1},
+    {"x": 1, "y": 3},
+    {"x": 1, "y": 2},
+    {"x": 2, "y": 4},
+]
+
+print(list(dequpe(a,key=lambda d: (d['x'],d['y']))))
