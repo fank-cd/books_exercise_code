f8503272fdff97af6ad8981e0a96937dce731426
fank-cd <2464512446@qq.com>
2020-04-04 00:08:14 +0800

    ch 8

---
+++ cookbook/Chapter 8/8,7调用父类中的方法.py
@@ -0,0 +1,27 @@
+"""
+super()表示从当前类中的MRO列表中（实际不是列表，是元组，因为只读）
+读取下一个基类去搜索
+
+有一些规则：
+1、确保在继承体系中所有同名的方法都有可兼容的调用签名（即参数数量相同，参数名称相同）
+2、确保顶层的类实现了这个方法通常是个好主意
+"""
+
+class A:
+    def spam(self):
+        print("A,spam")
+        
+        
+class B(A):
+    
+    def __init__():
+        
+    def spam(self):
+        print("B.spam")
+        super().spam()
+
+if __name__ == '__main__':
+    b = B()
+    b.spam()
+    
+    
---
+++ cookbook/Chapter 8/8.6创建可管理的属性.py
@@ -0,0 +1,36 @@
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
+
+    # Getter function
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Except a string")
+        self._first_name = value
+
+    @first_name.deleter
+    def first_name(self):
+        raise AttributeError("Can't delete attribute")
+# property 是python内置的装饰器，可以把类中的一个函数变成一个属性，
+# 使用property后就会生成 setter 和deleter。一个是用来设置值的，另一个是删除的
+# 他们可以根据场景不同来自由变化
+
+
+if __name__ == '__main__':
+    p = Person("fank")
+    print(p.first_name)  # fank
+    print(p._first_name)  # fank
+    print(p.first_name is p._first_name)  # True
+    p.first_name = 123  # Type Error
+    p = Person(123)  # Type Error
+
+    """
+    可以看出虽然init方法中不是_first_name 而我们返回 设置都是对_first_name 进行操作
+    但实际上创建实例的时候 仍然调用的setter方法，因此会跳过self.firt_name 去访问self._first_name
+    在这个例子中，对实例设置属性时进行类型检查 就是 property的全部意义
+    """
