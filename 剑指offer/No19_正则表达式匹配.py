945df644e349680be938779edebab8db826a49f0
fukaifeng <2464512446@qq.com>
2020-06-03 17:39:27 +0800

    添加19新写法

--- 剑指offer/No19_正则表达式匹配.py
+++ 剑指offer/No19_正则表达式匹配.py
@@ -36,3 +36,16 @@ def match_core(string,pattern):
 
 if __name__ == "__main__":
     print(match("aaaefwe", "aa*"))
+
+# 更优雅的写法
+
+def isMatch(text,pattern):
+    if not pattern:
+        return not text
+    first_match = bool(text) and pattern[0] in {text[0],"."}
+
+    if len(pattern) >= 2 and pattern[1] == "*":
+        return (isMatch(text,pattern[2:])) or first_match and isMatch(text[1:],pattern)
+
+    else:
+        return first_match and isMatch(text[1:],pattern[1:])
